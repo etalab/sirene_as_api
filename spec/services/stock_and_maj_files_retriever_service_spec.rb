e51e92da2003d8063d0a57914642819013e88262
Hugo Lepetit <giglemad@gmail.com>
2017-03-27 18:44:28 +0200

    Refactored spec and code for StockAndMajFilesRetrieverService

--- spec/services/stock_and_maj_files_retriever_service_spec.rb
+++ spec/services/stock_and_maj_files_retriever_service_spec.rb
@@ -3,18 +3,8 @@ require 'rails_helper'
 describe StockAndMajFilesRetrieverService do
 
   subject { described_class }
-
-  before do
-    Timecop.freeze(Time.new(2017,2,15,12))
-  end
-
-  it 'last_monthly_stock_zip_file_name' do
-    expect(subject.last_monthly_stock_zip_file_name).to eq('sirene_201702_L_M.zip')
-  end
-
-  describe '#needed_files' do
-    it 'when 15th day of month' do
-      expect(subject.needed_files.length).to eq(10)
-    end
+  context 'uses a VCR cassette to enable tests, else this is useless' do
+    it 'number of update files'
+    it 'last monthly stock'
   end
 end
