f8f823a4306e85ff9b2954241aa37f0bd6a3ddff
Hugo Lepetit <giglemad@gmail.com>
2017-03-30 13:22:47 +0200

    Added VCR, Test and implentation get_last_monthly_stock_link interactor

--- app/interactors/get_last_monthly_stock_link.rb
+++ app/interactors/get_last_monthly_stock_link.rb
@@ -1,7 +1,39 @@
+require 'nokogiri'
+require 'open-uri'
+
 class GetLastMonthlyStockLink
   include Interactor
 
   def call
-    # TODO
+    last_monthly_stock_relative_link = stock_relative_links.map{ |sl| sl[:href] }.sort.last
+    context.link = "#{files_domain}#{last_monthly_stock_relative_link}"
+  end
+
+  private
+  def stock_relative_links
+    sirene_update_and_stock_links.select do |l|
+      l[:href].match(sirene_monthly_stock_filename_pattern)
+    end
+  end
+
+  def sirene_update_and_stock_links
+    doc = Nokogiri::HTML(open(files_repository))
+    links = doc.css('a')
+  end
+
+  def files_domain
+    'http://files.data.gouv.fr'
+  end
+
+  def files_repository
+    "#{files_domain}/sirene"
+  end
+
+  def sirene_monthly_stock_filename_pattern
+    /.*sirene_#{current_year}([0-9]{2})_L_M\.zip/
+  end
+
+  def current_year
+    Time.now.year.to_s
   end
 end
