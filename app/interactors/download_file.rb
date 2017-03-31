0d8663b0c2daee2e0834a342cca0c8a7b4101fdd
Hugo Lepetit <giglemad@gmail.com>
2017-03-31 12:00:05 +0200

    Moved import_monthly_etablissements_stock_service.rb into interactors

--- app/interactors/download_file.rb
+++ app/interactors/download_file.rb
@@ -1,7 +1,16 @@
+require 'open-uri'
+
 class DownloadFile
   include Interactor
 
   def call
-    # TODO
+    filename = URI(context.link).path.split('/').last
+
+    context.filepath = "./public/#{filename}"
+
+    unless File.exists?(context.filepath)
+      download = open(context.link)
+      IO.copy_stream(download, context.filepath)
+    end
   end
 end
