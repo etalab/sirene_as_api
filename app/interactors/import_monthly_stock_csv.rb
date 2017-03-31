56b04670715344252de9f4d6025fc42839a4fbcd
Hugo Lepetit <giglemad@gmail.com>
2017-03-31 12:00:05 +0200

    Moved import_monthly_etablissements_stock_service.rb into interactors

--- app/interactors/import_monthly_stock_csv.rb
+++ app/interactors/import_monthly_stock_csv.rb
@@ -2,6 +2,54 @@ class ImportMonthlyStockCsv
   include Interactor
 
   def call
-    # TODO
+    quietly do
+      stdout_etablissement_count_change do
+        stdout_benchmark_stats do
+
+          binding.pry
+          SmarterCSV.process(context.unzipped_files.first, csv_options) do |chunk|
+            InsertEtablissementRowsJob.new(chunk).perform
+          end
+
+        end
+      end
+    end
+  end
+
+  def csv_options
+    {
+      chunk_size: 10000,
+      col_sep: ';',
+      row_sep: "\r\n",
+      convert_values_to_numeric: false,
+      key_mapping: {},
+      file_encoding: 'windows-1252'
+    }
+  end
+
+  def quietly
+    log_level_before_block_execution = Rails.logger.level
+
+    Rails.logger.level = :fatal
+    yield
+    Rails.logger.level = log_level_before_block_execution
+  end
+
+  def stdout_etablissement_count_change
+    etablissement_count_before = Etablissement.count
+    yield
+    etablissement_count_after = Etablissement.count
+
+    entries_added = etablissement_count_after - etablissement_count_before
+
+    puts "#{entries_added} etablissements added"
+  end
+
+  def stdout_benchmark_stats
+    Benchmark.bm(7) do |x|
+      x.report(:csv_pro) do
+        yield
+      end
+    end
   end
 end
--- app/interactors/unzip_file.rb
+++ app/interactors/unzip_file.rb
@@ -1,7 +1,31 @@
+require 'zip'
+
 class UnzipFile
   include Interactor
 
+  around do |interactor|
+    puts "------> Started unzipping #{context.filepath} into public/"
+    interactor.call
+    puts "------> SUCCESS File already existed or was unzipped properly #{context.filepath}"
+  end
+
   def call
-    # TODO
+    destination = 'public/'
+
+    context.unzipped_files = []
+
+    Zip::File.open(context.filepath) do |zip_file|
+      zip_file.each do |f|
+        csv_path = File.join(destination, f.name)
+
+        if File.exists?(csv_path)
+          context.unzipped_files << csv_path
+        else
+          zip_file.extract(f, csv_path)
+          context.unzipped_files << csv_path
+        end
+
+      end
+    end
   end
 end
