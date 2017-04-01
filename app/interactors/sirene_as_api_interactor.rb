5237b8169dad3732d0cba63def9202b3bd76518e
Hugo Lepetit <giglemad@gmail.com>
2017-04-01 17:55:14 +0200

    Boyscout

--- app/interactors/sirene_as_api_interactor.rb
+++ app/interactors/sirene_as_api_interactor.rb
@@ -5,19 +5,23 @@ class SireneAsAPIInteractor
     '------> '
   end
 
-  def check_mark
-    "\xE2\x9C\x93"
+  def stdout_info_log(msg)
+    puts "#{log_prefix}  #{msg.capitalize}"
   end
 
   def stdout_warn_log(msg)
-    puts msg.capitalize.yellow
+    puts seven_spaces + msg.capitalize.yellow
   end
 
   def stdout_success_log(msg)
-    puts "#{check_mark}  #{msg.capitalize}".green
+    puts seven_spaces + "#{check_mark}  #{msg.capitalize}".green
   end
 
-  def stdout_info_log(msg)
-    puts "#{log_prefix}  #{msg.capitalize}"
+  def seven_spaces
+    ' ' * 7
+  end
+
+  def check_mark
+    "\xE2\x9C\x93"
   end
 end
