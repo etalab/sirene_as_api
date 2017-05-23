231eedc25c36f67d8862a7cb4531b6ded9303af8
Zerhariel <sfaure@student.42.fr>
2017-05-23 13:09:47 +0200

    added default scope for etablissement model and tests

--- spec/factories/etablissement_factory.rb
+++ spec/factories/etablissement_factory.rb
@@ -1,8 +1,12 @@
 #TODO: Check validation siren dans github SIAD
+# Rappel : Mise a jour => Clef I/F
+# Creation : C, Entŕee diffusion : D
+# Suppression : E, Sortie diffusion : O
 
 FactoryGirl.define do
   factory :etablissement do
     sequence(:siren) { |n| "11111#{n}" }
     sequence(:date_mise_a_jour, ('00'..'59').cycle) { |n| "2017-01-01T10:25:#{n}" }
+    nature_mise_a_jour ["I", "F, ""C", "D", "E", "O"].sample
   end
 end
---
+++ spec/support/remove_indexing_solr.rb
@@ -0,0 +1,21 @@
+# TODO: Factorise for not having to copy model Etablissement every time
+class Etablissement < ApplicationRecord
+  attr_accessor :csv_path
+  default_scope -> { where(nature_mise_a_jour: ["I", "F, ""C", "D", "E"]) }
+
+  searchable auto_remove: false, auto_index: false do
+    text :nom_raison_sociale
+  end
+
+  # private
+  # def perform_index_tasks
+  #   nil
+  # end
+  def self.latest_mise_a_jour
+    latest_entry.date_mise_a_jour
+  end
+
+  def self.latest_entry
+    limit(1).order('date_mise_a_jour DESC').first
+  end
+end
