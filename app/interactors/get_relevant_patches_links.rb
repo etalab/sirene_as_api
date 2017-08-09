97a89cf8fc4eff7ca0d6d0e25049b1cf58d4d589
Zerhariel <sfaure@student.42.fr>
2017-08-09 17:16:27 +0200

    Updated README, fixed GetRelevantPatchesLink for edge case

--- app/interactors/get_relevant_patches_links.rb
+++ app/interactors/get_relevant_patches_links.rb
@@ -16,20 +16,18 @@ class GetRelevantPatchesLinks < SireneAsAPIInteractor
   end
 
   def call
-    relevant_patches_relative_links = select_all_patches_after_(padded_latest_etablissement_mise_a_jour_day_number)
+    relevant_patches_relative_links =
+      select_all_patches_after_(padded_latest_etablissement_mise_a_jour_day_number)
 
     unless context.we_are_rebuilding_database
-      if there_is_less_than_5_patches_since_LM
-        relevant_patches_relative_links = get_patches_since_LM
+      if there_is_less_than_5_patches_since_last_monthly_stock
+        relevant_patches_relative_links = patches_since_last_monthly_stock
       else
         relevant_patches_relative_links = get_minimum_5_patches(relevant_patches_relative_links)
       end
     end
 
-    relevant_patches_absolute_links = relevant_patches_relative_links.map do |relative_link|
-      "#{files_domain}#{relative_link[:href]}"
-    end
-    context.links = relevant_patches_absolute_links
+    context.links = change_into_absolute_links(relevant_patches_relative_links)
   end
 
   private
@@ -50,25 +48,33 @@ class GetRelevantPatchesLinks < SireneAsAPIInteractor
 
   def get_minimum_5_patches(links)
     if links.size.between?(1, 5)
-      stdout_info_log "#{links.size} patch found; last 5 patches will be applied."
+      stdout_info_log "#{links.size} new patch found; last 5 patches will be applied."
       select_all_patches.last(5)
     else
       links
     end
   end
 
-  def there_is_less_than_5_patches_since_LM
-    get_patches_since_LM.size < 5
+  def there_is_less_than_5_patches_since_last_monthly_stock
+    patches_since_last_monthly_stock.size < 5
   end
 
-  def get_patches_since_LM
+  def patches_since_last_monthly_stock
     # The following regexp catch the last 2 numbers of a string ergo the month
     last_month_stock_applied = /\d\d(?=[^\d])/.match(last_monthly_stock_name)[0]
-    current_month = last_month_stock_applied.to_i + 1
-    yday_beginning_current_month = Date.new(current_year.to_i, current_month, 01).yday
+    # The last month stock link is published be at the beginning of current month,
+    # modulo 12 for december->january
+    current_month = (last_month_stock_applied.to_i + 1) % 12
+    yday_beginning_current_month = Date.new(current_year.to_i, current_month, 1).yday
     beginning_current_month = yday_beginning_current_month.to_s.rjust(3, '0')
 
     select_all_patches_after_(beginning_current_month)
+  end
+
+  def change_into_absolute_links(relative_links)
+    relative_links.map! do |relative_link|
+      "#{files_domain}#{relative_link[:href]}"
+    end
   end
 
   def last_monthly_stock_name
@@ -77,7 +83,7 @@ class GetRelevantPatchesLinks < SireneAsAPIInteractor
 
   def sirene_update_and_stock_links
     doc = Nokogiri::HTML(open(files_repository))
-    links = doc.css('a')
+    doc.css('a') # Return links
   end
 
   def padded_latest_etablissement_mise_a_jour_day_number
@@ -86,7 +92,7 @@ class GetRelevantPatchesLinks < SireneAsAPIInteractor
       latest_etablissement_mise_a_jour = Etablissement.latest_mise_a_jour
       day_number = Date.parse(latest_etablissement_mise_a_jour).yday
       padded_day_number = day_number.to_s.rjust(3, '0')
-      stdout_info_log "Latest Etablissement in database is from patch #{padded_day_number}"
+      stdout_info_log "Latest Etablissement update in database is from patch #{padded_day_number}"
       padded_day_number
     end
   end
