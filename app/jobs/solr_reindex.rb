76061c94ec374feeded0dbe60ef50cf4b30ef401
Zerhariel <sfaure@student.42.fr>
2017-08-09 17:16:27 +0200

    Updated README, fixed GetRelevantPatchesLink for edge case

--- app/jobs/solr_reindex.rb
+++ app/jobs/solr_reindex.rb
@@ -2,7 +2,7 @@ class SolrReindex < SireneAsAPIInteractor
   around do
     if context.we_are_rebuilding_database || !context.links.empty?
       stdout_info_log('Database was modified. Starting Solr reindexing...')
-      `rake sunspot:reindex` # REVIEW: need to specify environnement ?
+      `rake sunspot:reindex` # REVIEW: need to specify environnement in production ?
     end
   end
 end
