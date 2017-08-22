290c81047b12d927454448bb03b01561a00df88a
Zerhariel <sfaure@student.42.fr>
2017-08-22 13:36:29 +0200

    Implemented spellchecking in fulltext controller, Corrected some TODO in backend

--- app/controllers/full_text_controller.rb
+++ app/controllers/full_text_controller.rb
@@ -1,16 +1,16 @@
+require 'sunspot'
+
 class FullTextController < ApplicationController
   def show
     @page = params[:page] || 1
-    @this_is_not_our_first_search = false
-
     spellcheck_search(params[:id])
   end
 
-  def spellcheck_search(keyword)
-    search = search_with_solr_options(keyword)
+  def spellcheck_search(query)
+    search = search_with_solr_options(query)
     results = search.results
 
-    unless results.blank?
+    if !results.blank?
       results_payload = {
         total_results: search.total,
         total_pages: results.total_pages,
@@ -20,12 +20,13 @@ class FullTextController < ApplicationController
       }
       render json: results_payload, status: 200
     else
-      if search.spellcheck_suggestion_for(keyword).blank? ||  @this_is_not_our_first_search
+      spellchecked_query = search.spellcheck_collation
+      # puts 'SPELLCHECK :'.red
+      # puts spellchecked_query
+      if spellchecked_query.nil?
         render json: { message: 'no results found' }, status: 404
       else
-        @this_is_not_our_first_search = true
-        spellchecked_keyword = search.spellcheck_suggestion_for(keyword)
-        spellcheck_search(spellchecked_keyword)
+        spellcheck_search(spellchecked_query)
       end
     end
   end
@@ -38,15 +39,47 @@ class FullTextController < ApplicationController
       facet :code_postal
       with(:code_postal, params[:code_postal]) if params[:code_postal].present?
 
-      spellcheck :count => 3
+      spellcheck :count => 5
 
       without(:nature_mise_a_jour).any_of(%w[O E]) # Scoping
       paginate page: @page, per_page: 10
     end
     search
   end
+end
 
-  def full_text_params
-    params.permit(:id, :page)
+# Modified Sunspot classes for making Spellcheck work.
+module Sunspot::Search
+  class StandardSearch
+    def spellcheck_suggestion_for(term)
+      if spellcheck_suggestions.nil? || spellcheck_suggestions[term].nil?
+        return nil
+      end
+      spellcheck_suggestions[term]['suggestion'].sort_by do |suggestion|
+        suggestion['freq']
+      end.last['word']
+    end
+
+    def spellcheck_collation(*terms)
+      if solr_spellcheck['suggestions'] && solr_spellcheck['suggestions'].length > 0
+        collation = terms.join(" ").dup if terms
+
+        if terms.length > 0
+          terms.each do |term|
+            if (spellcheck_suggestions[term]||{})['origFreq'] == 0
+              collation[term] = spellcheck_suggestion_for(term)
+            end
+          end
+        end
+
+        if terms.length == 0
+          collation = solr_spellcheck['collations'][-1]
+        end
+
+        collation
+      else
+        nil
+      end
+    end
   end
 end
--- spec/factories/etablissement_factory.rb
+++ spec/factories/etablissement_factory.rb
@@ -9,7 +9,7 @@ FactoryGirl.define do
     sequence(:siren) { |n| "11111#{n}" }
     sequence(:date_mise_a_jour, ('00'..'59').cycle) { |n| "2017-01-01T10:25:#{n}" }
     nature_mise_a_jour ["I", "F, ""C", "D"].sample
-    activite_principale ["APE1", "APE2", "APE3", "APE4", "APE5", "APE6"].sample
-    code_postal ["92600", "75015", "34000", "93000", "55055", "07300"].sample
+    activite_principale ["ACTIVITE1", "ACTIVITE2", "ACTIVITE3", "ACTIVITE4", "ACTIVITE5", "ACTIVITE6"].cycle
+    code_postal ["92600", "75015", "34000", "93000", "55055", "07300"].cycle
   end
 end
