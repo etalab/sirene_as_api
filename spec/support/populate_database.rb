5413f30f9bc102f4639aac676fcc013509f14e8f
Zerhariel <sfaure@student.42.fr>
2017-08-22 13:36:29 +0200

    Implemented spellchecking in fulltext controller, Corrected some TODO in backend

--- spec/support/populate_database.rb
+++ spec/support/populate_database.rb
@@ -12,21 +12,20 @@ def populate_test_database
   end
 end
 
-# TODO: Replace sample by array#cycle
 def populate_test_database_with_only_diffusion
   5.times do
-    create(:etablissement, nature_mise_a_jour: %w[I F C D].sample)
+    create(:etablissement, nature_mise_a_jour: %w[I F C D].cycle)
   end
 end
 
 def populate_test_database_with_no_diffusion
   2.times do
-    create(:etablissement, nature_mise_a_jour: %w[O E].sample)
+    create(:etablissement, nature_mise_a_jour: %w[O E].cycle)
   end
 end
 
 def populate_test_database_with_all
   10.times do
-    create(:etablissement, nature_mise_a_jour: %w[I F C D E O].sample)
+    create(:etablissement, nature_mise_a_jour: %w[I F C D E O].cycle)
   end
 end
