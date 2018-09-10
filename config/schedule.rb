bafc7fa418b060ec04ecb66509aaa2710a12a286
Alexis Leclerc <pihaelle@gmail.com>
2018-09-10 11:52:29 +0300

    [Whenever] correct script calls

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,26 @@
 # Learn more: http://github.com/javan/whenever
 set :output, File.join(Whenever.path, 'log', 'rna_api_cron.log')
 
-every 1.day, at: '4:30 am' do
-  rake 'sirene_as_api:dual_server_update'
+###### SANDBOX ######
+
+if Rails.env.sandbox?
+  every 2.days, at: '8:00 am' do
+    rake 'sirene_as_api:automatic_update_database'
+  end
+end
+
+###### PRODUCTION ######
+
+if Rails.env.production?
+  # CRON Job for single server update, uncomment if you have a single server
+
+  # every 1.day, at: '4:30 am' do
+  #   rake 'sirene_as_api:automatic_update_database'
+  # end
+
+  # CRON jobs for dual server update, comment out if you have a single server
+  # The rake task is launched only if the server is not used, so each server will update every other day
+  every 1.day, at: '4:30 am' do
+    rake 'sirene_as_api:dual_server_update'
+  end
 end
