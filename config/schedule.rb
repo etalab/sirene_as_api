3cdbddcfa60485f45efc032d0a5d7e1b1500db5d
Alexis Leclerc <alexis@alxs.fr>
2018-09-18 15:29:08 +0300

    [Solr] improve deploy closes #111

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,19 @@
 # Learn more: http://github.com/javan/whenever
 set :output, File.join(Whenever.path, 'log', 'rna_api_cron.log')
 
-###### SANDBOX ######
+# environment var is set with
+# whenever --set 'environment=VALUE'
+# which is set by Mina/whenver with RAILS_ENV
 
-if Rails.env.sandbox?
+###### SANDBOX ######
+if environment == 'sandbox'
   every 2.days, at: '8:00 am' do
     rake 'sirene_as_api:automatic_update_database'
   end
 end
 
 ###### PRODUCTION ######
-
-if Rails.env.production?
+if environment == 'production'
   # CRON Job for single server update, uncomment if you have a single server
 
   # every 1.day, at: '4:30 am' do
