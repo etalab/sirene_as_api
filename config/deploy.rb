8385f62e49dc22dbddbb3aeb924f8610821b8bc1
Alexis Leclerc <alexis@alxs.fr>
2018-09-30 17:41:16 +0300

    [Mina] scumbag .rb missing

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ set :shared_dirs, fetch(:shared_dirs, []).push(
 
 set :shared_files, fetch(:shared_files, []).push(
   'config/database.yml',
-  "config/environments/#{ENV['to']}",
+  "config/environments/#{ENV['to']}.rb",
   'config/secrets.yml'
 )
 
