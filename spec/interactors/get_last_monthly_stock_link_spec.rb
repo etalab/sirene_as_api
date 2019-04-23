c683d29916f26c63f9fbf7fda2942b7f25e936d3
Alexis Leclerc <alexis@alxs.fr>
2019-04-23 14:50:59 +0300

    [VCR] move allow_playback_repeats to spec_helper

--- spec/interactors/get_last_monthly_stock_link_spec.rb
+++ spec/interactors/get_last_monthly_stock_link_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe GetLastMonthlyStockLink do
   subject(:context) { described_class.call}
 
-  context 'when file server is down', vcr: { cassette_name: 'geo-sirene_server_KO', allow_playback_repeats: true } do
+  context 'when file server is down', vcr: { cassette_name: 'geo-sirene_server_KO' } do
     it 'fails' do
       expect(context).to be_a_failure
     end
   end
 
-  context 'when in march', vcr: { cassette_name: 'geo-sirene_march', allow_playback_repeats: true } do
+  context 'when in march', vcr: { cassette_name: 'geo-sirene_march' } do
     it 'retrieves february stock link' do
       allow(Time).to receive(:now) { Time.new(2018, 3, 31) }
 
@@ -18,7 +18,7 @@ describe GetLastMonthlyStockLink do
     end
   end
 
-  context 'when in march but link is not available', vcr: { cassette_name: 'geo-sirene_march_link_not_available', allow_playback_repeats: true } do
+  context 'when in march but link is not available', vcr: { cassette_name: 'geo-sirene_march_link_not_available' } do
     it 'fails' do
       allow(Time).to receive(:now) { Time.new(2018, 3, 31) }
 
@@ -26,7 +26,7 @@ describe GetLastMonthlyStockLink do
     end
   end
 
-  context 'When on the 1st of april and no stock link yet', vcr: { cassette_name: 'geo-sirene_1_april_no_stock_yet', allow_playback_repeats: true } do
+  context 'When on the 1st of april and no stock link yet', vcr: { cassette_name: 'geo-sirene_1_april_no_stock_yet' } do
     it 'retrieves february stock link' do
       allow(Time).to receive(:now) { Time.new(2018, 4, 1) }
 
@@ -35,7 +35,7 @@ describe GetLastMonthlyStockLink do
     end
   end
 
-  context 'when in january', vcr: { cassette_name: 'geo-sirene_january_2018', allow_playback_repeats: true } do
+  context 'when in january', vcr: { cassette_name: 'geo-sirene_january_2018' } do
     it 'retrieves the monthly stock from december the previous year' do
       allow(Time).to receive(:now) { Time.new(2018, 1, 5) }
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,6 +42,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.default_cassette_options[:allow_playback_repeats] = true
   # Config allow http connections without cassettes for requests on Solr server
   config.allow_http_connections_when_no_cassette = true
   # Config ignore_request to stop VCR from managing Solr server requests
