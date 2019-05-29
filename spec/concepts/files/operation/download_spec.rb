89f866de1f78ffb3d7dc3be761ee3f15e8d59b13
Alexis Leclerc <alexis@alxs.fr>
2019-05-29 17:33:20 +0300

    Add Download operation

---
+++ spec/concepts/files/operation/download_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe Files::Operation::Download do
+  subject { described_class.call uri: uri, logger: logger }
+
+  let(:logger) { instance_double(Logger).as_null_object }
+
+  context 'downloads success', vcr: { cassette_name: 'download_success' } do
+
+    after do
+      File.delete Rails.root.join 'tmp', 'files', filename
+    end
+
+    let(:url) { 'http://www.ovh.net/files/' }
+    let(:filename) { '1Mb.dat' }
+    let(:uri) { url + filename }
+    let(:local_file_path) { Rails.root.join 'tmp', 'files', filename }
+
+    it { is_expected.to be_success }
+
+    its([:file_path]) { is_expected.to eq local_file_path.to_s }
+
+    it 'downloads the file on disk' do
+      expect(File.exists?(subject[:file_path])).to be_truthy
+    end
+  end
+
+  context 'when download fails', vcr: { cassette_name: 'download_failed' } do
+    let(:uri) { 'https://entreprise.api.gouv.fr/not_found.lol' }
+
+    it { is_expected.to be_failure }
+
+    it 'logs an error' do
+      expect(logger).to receive(:error).with("Download failed: 404 Not Found")
+      subject
+    end
+  end
+end
