4f06c3978f70d84b750f0e7892f70a3ac85dc90c
Alexis Leclerc <alexis@alxs.fr>
2019-06-04 16:13:35 +0300

    [RSpec] auto import support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,9 +8,6 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'ruby-progressbar'
-require 'support/factory_bot'
-require 'support/populate_database'
-require 'support/custom_matchers/json_matchers.rb'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -25,7 +22,7 @@ require 'support/custom_matchers/json_matchers.rb'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
