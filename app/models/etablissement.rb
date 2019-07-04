61138bf26a8f1fb4dd0dc99b2620b07876030387
Samson Zhang <smnzhang@hotmail.com>
2019-07-04 14:34:34 +0200

    speed up last update date retrieval from db

--- app/models/etablissement.rb
+++ app/models/etablissement.rb
@@ -28,10 +28,18 @@ class Etablissement < ApplicationRecord
   end
 
   def self.latest_mise_a_jour
-    latest_entry.try(:date_mise_a_jour)
+    # probabilist search for true latest update. Date time must be exact but a wrong time is tolerated
+    # always works if at least one new entry had been inserted the previous day otherwise works well in most cases (>99.9% confidence)
+    # 20 000 can be a good upper bound since every patch seems to have less than 20 000 lines
+    last_update_nb_entries_tol = 20_000
+
+    sql = "SELECT max(date_mise_a_jour)
+           FROM (SELECT date_mise_a_jour FROM etablissements
+                 ORDER BY id DESC
+                 LIMIT #{last_update_nb_entries_tol}
+           ) as x"
+
+    ActiveRecord::Base.connection.exec_query(sql).first["max"]
   end
 
-  def self.latest_entry
-    limit(1).order('date_mise_a_jour DESC').first
-  end
 end
