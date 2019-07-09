583e0ce6116bc6a22e16cf7c21842965f6fbc948
Samuelfaure <samuel.faure.dev@gmail.com>
2019-07-09 13:23:29 +0200

    better factorisation of scopable

--- app/controllers/concerns/scopable.rb
+++ app/controllers/concerns/scopable.rb
@@ -2,10 +2,19 @@ module Scopable
   module Controller
     extend ActiveSupport::Concern
 
-    # Add has_scope for each model attribute
     included do
-      controller_name.classify.constantize.attribute_names.each do |a|
-        has_scope a.to_sym, ->(value) { where(Hash[a, value]) }, only: :index
+      controller_attributes.each { |attr| add_has_scope_for_attribute(attr) }
+    end
+
+    module ClassMethods
+      private
+
+      def controller_attributes
+        controller_name.classify.constantize.attribute_names
+      end
+
+      def add_has_scope_for_attribute(attr)
+        has_scope(attr.to_sym, ->(value) { where(Hash[attr, value]) }, only: :index)
       end
     end
   end
@@ -13,10 +22,15 @@ module Scopable
   module Model
     extend ActiveSupport::Concern
 
-    # Add scope for each model attribute
     included do
-      self.attribute_names.each do |a|
-        scope a.to_sym, ->(value) { where(Hash[a, value]) }
+      attribute_names.each { |attr| add_scope_for_attribute(attr) }
+    end
+
+    module ClassMethods
+      private
+
+      def add_scope_for_attribute(attr)
+        scope attr.to_sym, ->(value) { where(Hash[attr, value]) }
       end
     end
   end
