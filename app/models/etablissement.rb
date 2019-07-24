c5371af69400b118cc6667b7d25692aa3aea99ae
Samuelfaure <samuel.faure.dev@gmail.com>
2019-07-24 14:55:58 +0200

    factorized scopable concern

--- app/models/etablissement.rb
+++ app/models/etablissement.rb
@@ -1,6 +1,5 @@
 class Etablissement < ApplicationRecord
   include Scopable::Model
-  
   belongs_to :unite_legale, optional: true
 
   def self.header_mapping
--- spec/support/shared_examples/scopable.rb
+++ spec/support/shared_examples/scopable.rb
@@ -1,36 +1,25 @@
-shared_examples 'scopable' do |model, field_1, field_2|
+shared_examples 'a scopable controller' do |model, field_1, field_2|
   describe 'filtering', type: :request do
-    let!(:instance_1) { create(model, field_1 => '001', field_2 => 'Foo') }
-    let!(:instance_2) { create(model, field_1 => '002', field_2 => 'Bar') }
-    let!(:instance_3) { create(model, field_1 => '003', field_2 => 'Bar') }
-
-    let(:route) { "/v3/#{model.to_s.pluralize}" }
-
     it 'can filter with 1 field' do
       get "#{route}?#{field_1.to_s}=001"
 
-      response_array = JSON.parse(response.body)
-
-      expect(response_array.size).to eq(1)
-      expect(response_array.first[field_2.to_s]).to eq('Foo')
+      expect(number_of_results).to eq(1)
+      expect(first_result[field_2.to_s]).to eq('Foo')
     end
 
     it 'can filter with multiple fields' do
       get "#{route}?#{field_1.to_s}=001&#{field_2.to_s}=Foo"
 
-      response_array = JSON.parse(response.body)
-
-      expect(response_array.size).to eq(1)
-      expect(response_array.first[field_2.to_s]).to eq('Foo')
+      expect(number_of_results).to eq(1)
+      expect(first_result[field_2.to_s]).to eq('Foo')
     end
 
     it 'can filter and return multiple results' do
-      get "#{route}?#{field_2.to_s}=Bar"
+      get "#{route}?#{field_2.to_s}=Foo"
 
-      response_array = JSON.parse(response.body)
-
-      expect(response_array.size).to eq(2)
-      expect(response_array.first[field_2.to_s]).to eq('Bar')
+      expect(number_of_results).to eq(2)
+      expect(first_result[field_2.to_s]).to eq('Foo')
+      expect(second_result[field_2.to_s]).to eq('Foo')
     end
   end
 end
