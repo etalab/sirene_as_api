fbd24049c1702cc4a99eecd1c930fe538a379745
Alexis Leclerc <alexis@alxs.fr>
2019-08-01 14:04:00 +0300

    Post Review: change redis port & schdule sandbox/prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,19 @@
-schedule_file = "config/schedule.yml"
+def load_sidekiq_cron_jobs
+  schedule_file = 'config/schedule.yml'
 
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  if File.exists?(schedule_file)
+    loaded_conf = YAML.load_file(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash(loaded_conf[Rails.env])
+  end
+end
+
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: Rails.configuration.redis_database }
+
+  load_sidekiq_cron_jobs
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: Rails.configuration.redis_database }
 end
