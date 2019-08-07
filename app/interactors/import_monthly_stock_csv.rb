0f73bd3a2ce3d9c11ed7fa7cfe47871de97eb9f4
Samuelfaure <samuel.faure.dev@gmail.com>
2019-08-07 16:21:53 +0200

    fixed table name and unnecessary option

--- app/interactors/import_monthly_stock_csv.rb
+++ app/interactors/import_monthly_stock_csv.rb
@@ -54,8 +54,7 @@ class ImportMonthlyStockCsv < SireneAsAPIInteractor
     {
       chunk_size: 2_000,
       col_sep: ',',
-      row_sep: "\n",
-      header_transformations: [key_mapping: key_mapping_v2]
+      row_sep: "\n"
     }
   end
 
@@ -86,88 +85,5 @@ class ImportMonthlyStockCsv < SireneAsAPIInteractor
         yield
       end
     end
-  end
-
-  def key_mapping_v2
-    {
-      numvoie: :numero_voie,
-      indrep: :indice_repetition,
-      typvoie: :type_voie,
-      libvoie: :libelle_voie,
-      codpos: :code_postal,
-      rpet: :region,
-      libreg: :libelle_region,
-      depet: :departement,
-      arronet: :arrondissement,
-      ctonet: :canton,
-      comet: :commune,
-      libcom: :libelle_commune,
-      du: :departement_unite_urbaine,
-      tu: :taille_unite_urbaine,
-      uu: :numero_unite_urbaine,
-      epci: :etablissement_public_cooperation_intercommunale,
-      tcd: :tranche_commune_detaillee,
-      zemet: :zone_emploi,
-      siege: :is_siege,
-      ind_publipo: :indicateur_champ_publipostage,
-      diffcom: :statut_prospection,
-      amintret: :date_introduction_base_diffusion,
-      natetab: :nature_entrepreneur_individuel,
-      libnatetab: :libelle_nature_entrepreneur_individuel,
-      apet700: :activite_principale,
-      libapet: :libelle_activite_principale,
-      dapet: :date_validite_activite_principale,
-      tefet: :tranche_effectif_salarie,
-      libtefet: :libelle_tranche_effectif_salarie,
-      efetcent: :tranche_effectif_salarie_centaine_pret,
-      defet: :date_validite_effectif_salarie,
-      origine: :origine_creation,
-      dcret: :date_creation,
-      ddebact: :date_debut_activite,
-      activnat: :nature_activite,
-      lieuact: :lieu_activite,
-      actisurf: :type_magasin,
-      saisonat: :is_saisonnier,
-      modet: :modalite_activite_principale,
-      prodet: :caractere_productif,
-      prodpart: :participation_particuliere_production,
-      auxilt: :caractere_auxiliaire,
-      nomen_long: :nom_raison_sociale,
-      rna: :numero_rna,
-      nicsiege: :nic_siege,
-      rpen: :region_siege,
-      depcomen: :departement_commune_siege,
-      adr_mail: :email,
-      nj: :nature_juridique_entreprise,
-      libnj: :libelle_nature_juridique_entreprise,
-      apen700: :activite_principale_entreprise,
-      libapen: :libelle_activite_principale_entreprise,
-      dapen: :date_validite_activite_principale_entreprise,
-      aprm: :activite_principale_registre_metier,
-      ess: :is_ess,
-      dateess: :date_ess,
-      tefen: :tranche_effectif_salarie_entreprise,
-      libtefen: :libelle_tranche_effectif_salarie_entreprise,
-      efencent: :tranche_effectif_salarie_entreprise_centaine_pret,
-      defen: :date_validite_effectif_salarie_entreprise,
-      categorie: :categorie_entreprise,
-      dcren: :date_creation_entreprise,
-      amintren: :date_introduction_base_diffusion_entreprise,
-      monoact: :indice_monoactivite_entreprise,
-      moden: :modalite_activite_principale_entreprise,
-      proden: :caractere_productif_entreprise,
-      esaann: :date_validite_rubrique_niveau_entreprise_esa,
-      tca: :tranche_chiffre_affaire_entreprise_esa,
-      esaapen: :activite_principale_entreprise_esa,
-      esasec1n: :premiere_activite_secondaire_entreprise_esa,
-      esasec2n: :deuxieme_activite_secondaire_entreprise_esa,
-      esasec3n: :troisieme_activite_secondaire_entreprise_esa,
-      esasec4n: :quatrieme_activite_secondaire_entreprise_esa,
-      vmaj: :nature_mise_a_jour,
-      vmaj1: :indicateur_mise_a_jour_1,
-      vmaj2: :indicateur_mise_a_jour_2,
-      vmaj3: :indicateur_mise_a_jour_3,
-      datemaj: :date_mise_a_jour
-    }
   end
 end
