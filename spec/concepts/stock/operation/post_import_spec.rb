3ee69d5d24845056b2f7b31f91afb2b6f089da5b
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2019-11-27 17:48:59 +0200

    Update PostImport with new logic

--- spec/concepts/stock/operation/post_import_spec.rb
+++ spec/concepts/stock/operation/post_import_spec.rb
@@ -4,9 +4,6 @@ describe Stock::Operation::PostImport, :trb do
   subject { described_class.call logger: logger }
 
   let(:logger) { instance_spy Logger }
-  let(:siren) { '005880034' }
-  let!(:unite_legale) { create :unite_legale, siren: siren }
-  let!(:etablissement) { create :etablissement, siren: siren }
 
   shared_examples 'not doing anything' do
     it { is_expected.to be_success }
@@ -18,8 +15,7 @@ describe Stock::Operation::PostImport, :trb do
 
     it 'does not create relationship' do
       subject
-      expect(unite_legale.etablissements).to be_empty
-      expect(etablissement.unite_legale).to be_nil
+      expect_not_to_call_nested_operation(Stock::Task::CreateAssociations)
     end
 
     it 'does not create database indexes' do
@@ -64,63 +60,24 @@ describe Stock::Operation::PostImport, :trb do
 
     it { is_expected.to be_success }
 
-    it 'logs association starts' do
-      subject
-      expect(logger).to have_received(:info).with('Models associations starts')
-    end
-
-    it 'logs associations done' do
-      subject
-      expect(logger).to have_received(:info).with('Models associations completed')
-    end
-
     it 'created the association' do
+      expect_to_call_nested_operation(Stock::Task::CreateAssociations)
       subject
-      unite_legale.reload
-      etablissement.reload
-      expect(unite_legale.etablissements.count).to eq 1
-      expect(etablissement.unite_legale).to eq unite_legale
+    end
+
+    it 'swaps table names' do
+      expect_to_call_nested_operation(Stock::Task::SwapTableNames)
+      subject
+    end
+
+    it 'drops indexes' do
+      expect_to_call_nested_operation(Stock::Task::DropIndexes)
+      subject
     end
 
     it 'creates database indexes' do
       expect_to_call_nested_operation(Stock::Task::CreateIndexes)
       subject
-    end
-
-    context 'when association failed' do
-      before do
-        allow_any_instance_of(described_class)
-          .to receive(:sql)
-          .and_return 'an invalid SQL statement'
-      end
-
-      it { is_expected.to be_failure }
-
-      it 'logs import starts' do
-        subject
-        expect(logger).to have_received(:info).with('Models associations starts')
-      end
-
-      it 'logs an error' do
-        subject
-        expect(logger).to have_received(:error).with(/Association failed:/)
-      end
-    end
-
-    context 'when Index creation failed' do
-      before do
-        allow_any_instance_of(Stock::Task::CreateIndexes)
-          .to receive(:create_indexes)
-          .and_return false
-      end
-
-      it { is_expected.to be_failure }
-
-      it 'does not create associations' do
-        expect_any_instance_of(described_class)
-          .not_to receive(:sql)
-        subject
-      end
     end
   end
 end
