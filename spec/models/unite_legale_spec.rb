37d0f63507b7ed2e42e8460afae28a7d6457abaf
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2020-02-23 18:44:53 +0200

    Models: method to nullify unauthorized attributes

--- spec/models/unite_legale_spec.rb
+++ spec/models/unite_legale_spec.rb
@@ -6,4 +6,36 @@ describe UniteLegale do
   it 'has header_mapping' do
     expect(described_class.header_mapping).to include 'statutDiffusionUniteLegale' => :statut_diffusion
   end
+
+  describe '#nullify_non_diffusable_fields' do
+    context 'when diffusable' do
+      subject { create :unite_legale }
+
+      it 'does not nullify any fields' do
+        copy = subject.as_json
+        subject.nullify_non_diffusable_fields
+        expect(subject.as_json).to eq copy
+      end
+    end
+
+    context 'when non-diffusable' do
+      subject { create :unite_legale, :non_diffusable }
+
+      it 'nullify unauthorized fields' do
+        subject.nullify_non_diffusable_fields
+        subject.attributes.keys.each do |attr|
+          next if described_class::AUTHORIZED_FIELDS.include?(attr)
+
+          expect(subject.send(attr)).to be_nil
+        end
+      end
+
+      it 'does not nullify authorized fields' do
+        subject.nullify_non_diffusable_fields
+        described_class::AUTHORIZED_FIELDS.each do |attr|
+          expect(subject.send(attr)).not_to be_nil
+        end
+      end
+    end
+  end
 end
