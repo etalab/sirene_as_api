e3f3eb2c42959b8e76f4d508172eac6808aebb2f
Alexis Leclerc <alexis@alxs.fr>
2020-04-06 17:36:49 +0300

    Revert "Remove Dual server update logic"
    This reverts commit 27011cd65738d4c31d67483041102dbf5a5de51e.

---
+++ spec/interactors/check_current_service_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'net/http'
+
+describe CheckCurrentService do
+  include_context 'mute interactors'
+
+  context 'When this machine is in service',
+          vcr: { cassette_name: 'OvhAPI_check_current_service' } do
+    subject(:context) { described_class.call }
+    it 'fails the context' do
+      allow_any_instance_of(described_class).to receive(:current_machine_name).and_return('address1')
+      allow_any_instance_of(OvhAPI).to receive(:signature).and_return('dummy-sign')
+
+      described_class.call
+      expect(context).to be_a_failure
+    end
+  end
+
+  context 'When this machine is not in service',
+          vcr: { cassette_name: 'OvhAPI_check_current_service' } do
+    subject(:context) { described_class.call }
+    it 'succeed the context' do
+      allow_any_instance_of(described_class).to receive(:current_machine_name).and_return('address2')
+      allow_any_instance_of(OvhAPI).to receive(:signature).and_return('dummy-sign')
+
+      described_class.call
+      expect(context).to be_a_success
+    end
+  end
+end
