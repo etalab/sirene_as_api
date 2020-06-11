a53ab994ebe1e8f6d2c5f3d70983d07bec5b56f5
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2020-06-11 12:42:36 +0200

    Update specs for UpdateDatabse

--- spec/concepts/stock/operation/update_database_spec.rb
+++ spec/concepts/stock/operation/update_database_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
-describe Stock::Operation::UpdateDatabase, :trb, vcr: { cassette_name: 'update_database' } do
+describe Stock::Operation::UpdateDatabase, :trb do
   subject { described_class.call logger: logger }
 
   let(:logger) { instance_spy Logger }
 
   shared_examples 'both imports succeed' do
     it { is_expected.to be_success }
+
+    it 'calls CheckStockAvailability' do
+      expect_to_call_nested_operation(Stock::Operation::CheckStockAvailability)
+      subject
+    end
 
     it 'calls LoadUniteLegale' do
       expect_to_call_nested_operation(Stock::Operation::LoadEtablissement)
@@ -19,23 +24,24 @@ describe Stock::Operation::UpdateDatabase, :trb, vcr: { cassette_name: 'update_d
     end
   end
 
-  context 'when database is empty' do
+  context 'when database is empty', vcr: { cassette_name: 'update_empty_database' } do
     it_behaves_like 'both imports succeed'
   end
 
   # issue : https://github.com/etalab/sirene_as_api/issues/229
   # StockEtablissement corrupted for few days import never completed
-  context 'when UniteLegale already imported but not Etablissement' do
+  context 'when UniteLegale already imported but not Etablissement', vcr: { cassette_name: 'update_database' } do
     before do
-      create :stock_unite_legale, :of_july, :completed
-      create :stock_etablissement, :of_july, :errored
+      Timecop.freeze('2020-06-11')
+      create :stock_unite_legale, :of_june, :completed, year: '2020'
+      create :stock_etablissement, :of_june, :errored, year: '2020'
     end
 
     it 'log a warning unite legale is ignored' do
       subject
       expect(logger)
         .to have_received(:warn)
-        .with('Latest stock available (from month 07) already exists with status COMPLETED')
+        .with('Latest stock available (from month 06) already exists with status COMPLETED')
     end
 
     it_behaves_like 'both imports succeed'
